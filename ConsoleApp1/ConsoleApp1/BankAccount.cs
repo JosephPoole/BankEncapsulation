ad9187b8f06919567e376fcce5e4fd92bd48c198
Joseph Poole <Joseph.poole90@gmail.com>
2022-07-14 14:32:09 -0500

    First commit

---
+++ ConsoleApp1/ConsoleApp1/BankAccount.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountEnscapulation
+{
+    public class BankAccount
+    {
+        private static double balance = 00;
+        public static double GetBalance()
+        {
+            Console.WriteLine($"Your Balance is ${balance}");
+            return balance;
+        }
+
+        public static void Deposit(double amount)
+        {
+            balance += amount;
+        }
+
+        public static void Withdrawl(double amount)
+        { 
+            balance -= amount;
+        }
+
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿using BankAccountEnscapulation;
+
+var account1 = new BankAccount();
+Console.WriteLine("Please inut the amount you'd like to deposit.");
+double userinput = System.Convert.ToInt32(Console.ReadLine());
+BankAccount.Deposit(userinput);
+BankAccount.GetBalance();
+Console.WriteLine("How Much would you like to withdraw?");
+double userinput2 = System.Convert.ToInt32(Console.ReadLine());
+BankAccount.Withdrawl(userinput2);
+BankAccount.GetBalance();
